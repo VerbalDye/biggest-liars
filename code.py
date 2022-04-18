5166dc4d57e9737264afe0125a0f2ea11468f5c8
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-18 15:14:42 -0400

    finally lists prime numbers using star witnesses

--- code.py
+++ code.py
@@ -19,9 +19,22 @@ for n in range(5,101,2):
     for a in aList:
         results[len(results)-1].append([])
         for r in range(m):
-            c = a
-            for j in range(2^r * (d-1)):
+            c = 1
+            for j in range(2 ** r * d):
                 c = (c * a) % n
             results[len(results)-1][len(results[len(results)-1])-1].append(c)
 
-print(results)
+for number in results:
+    # print(number)
+    xTrue = False
+    yTrue = False
+    for x in number[1]:
+        if x == 1 or x == number[0]-1:
+            xTrue = True
+    for y in number[2]:
+        if y == 1 or y == number[0]-1:
+            yTrue = True
+    if yTrue and xTrue:
+        print(number[0])
+
+# print(results)
