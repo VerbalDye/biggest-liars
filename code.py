87261b085d1433f29c56e04e56fceadcac7e56d0
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-18 11:35:44 -0400

    getting closer need to fix m > 1 cases

--- code.py
+++ code.py
@@ -15,10 +15,13 @@ for n in range(5,101,2):
         else:
             d = math.floor(d)
             i = False
-    results.append({})
+    results.append([n])
     for a in aList:
-        for r in range(1, m + 1):
+        results[len(results)-1].append([])
+        for r in range(m):
             c = a
             for j in range(2^r * (d-1)):
                 c = (c * a) % n
-            results[len(results)]
+            results[len(results)-1][len(results[len(results)-1])-1].append(c)
+
+print(results)
